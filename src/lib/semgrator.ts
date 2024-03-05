4b7f55c9e3c48bc18bf90125be8633b6a2375eab
Matteo Collina <hello@matteocollina.com>
2024-03-05 15:38:15 +0100

    basic applying of semver ranges
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- src/lib/semgrator.ts
+++ src/lib/semgrator.ts
@@ -1,1 +1,32 @@
-export async function semgrator() {}
+import semver from 'semver'
+
+export type Migration<Input, Output = Input> = {
+  version: string
+  up: (input: Input) => Promise<Output> | Output
+}
+
+interface SemgratorParams<Input> {
+  version: string
+  migrations: Migration<any, any>[]
+  input: Input
+}
+
+interface SemgratorResult<Output> {
+  version: string
+  result: Output
+}
+
+export async function semgrator<Input = unknown, Output = unknown>(
+  params: SemgratorParams<Input>,
+): Promise<SemgratorResult<Output>> {
+  let result = params.input as unknown
+  let lastVersion = params.version
+  for (const migration of params.migrations) {
+    if (semver.gt(migration.version, lastVersion)) {
+      result = await migration.up(result)
+      lastVersion = migration.version
+    }
+  }
+
+  return { version: lastVersion, result: result as Output }
+}
--- src/test/semgrator.test.ts
+++ src/test/semgrator.test.ts
@@ -1,6 +1,128 @@
 import { test } from 'node:test'
-import { semgrator } from '../lib/semgrator.js'
+import { semgrator, Migration } from '../lib/semgrator.js'
+import { tspl } from '@matteo.collina/tspl'
 
-test('semgrator', async t => {
-  await semgrator()
+type SeenBy = {
+  foo: string
+  seenBy?: string
+}
+
+test('apply all  migrations', async t => {
+  const plan = tspl(t, { plan: 3 })
+  const m1: Migration<SeenBy> = {
+    version: '2.0.0',
+    async up(input) {
+      return {
+        ...input,
+        seenBy: '2.0.0',
+      }
+    },
+  }
+
+  const m2: Migration<SeenBy> = {
+    version: '2.4.0',
+    up(input) {
+      plan.deepEqual(input, {
+        foo: 'bar',
+        seenBy: '2.0.0',
+      })
+      return {
+        ...input,
+        seenBy: '2.4.0',
+      }
+    },
+  }
+
+  const res = await semgrator<SeenBy, SeenBy>({
+    version: '1.0.0',
+    migrations: [m1, m2],
+    input: {
+      foo: 'bar',
+    },
+  })
+
+  plan.equal(res.version, '2.4.0')
+  plan.deepEqual(res.result, {
+    foo: 'bar',
+    seenBy: '2.4.0',
+  })
+})
+
+test('apply only needed', async t => {
+  const plan = tspl(t, { plan: 3 })
+  const m1: Migration<SeenBy> = {
+    version: '2.0.0',
+    async up(input) {
+      plan.fail('should not be called')
+      return {
+        ...input,
+        seenBy: '2.0.0',
+      }
+    },
+  }
+
+  const m2: Migration<SeenBy> = {
+    version: '2.4.0',
+    up(input) {
+      plan.deepEqual(input, {
+        foo: 'bar',
+      })
+      return {
+        ...input,
+        seenBy: '2.4.0',
+      }
+    },
+  }
+
+  const res = await semgrator<SeenBy, SeenBy>({
+    version: '2.1.1',
+    migrations: [m1, m2],
+    input: {
+      foo: 'bar',
+    },
+  })
+
+  plan.equal(res.version, '2.4.0')
+  plan.deepEqual(res.result, {
+    foo: 'bar',
+    seenBy: '2.4.0',
+  })
+})
+
+test('apply no migrations', async t => {
+  const plan = tspl(t, { plan: 2 })
+  const m1: Migration<SeenBy> = {
+    version: '2.0.0',
+    async up(input) {
+      plan.fail('should not be called')
+      return {
+        ...input,
+        seenBy: '2.0.0',
+      }
+    },
+  }
+
+  const m2: Migration<SeenBy> = {
+    version: '2.4.0',
+    up(input) {
+      plan.fail('should not be called')
+      return {
+        ...input,
+        seenBy: '2.4.0',
+      }
+    },
+  }
+
+  const res = await semgrator<SeenBy, SeenBy>({
+    version: '2.30.1',
+    migrations: [m1, m2],
+    input: {
+      foo: 'bar',
+    },
+  })
+
+  plan.equal(res.version, '2.30.1')
+  plan.deepEqual(res.result, {
+    foo: 'bar',
+  })
 })
